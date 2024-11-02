3a03024d465083867b758cba40e79cf4b67a45dd
goorm <goorm@goormui-MacBookPro-5.local>
2024-11-03 00:16:35 +0900

    생각해보니 loginManager.js 만드는게 좋을듯...

---
+++ public/js/loginManager.js
@@ -0,0 +1,9 @@
+document.getElementById('loginButton').addEventListener('click', () => {
+    console.log("로그인 클릭")
+    window.location.href = "./posts.html"
+});
+
+document.getElementById('registButton').addEventListener('click', () => {
+    console.log("회원가입 클릭")
+    window.location.href = "./regist.html"
+});
