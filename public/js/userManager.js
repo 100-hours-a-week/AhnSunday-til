bfb2eb002a54b38f646a3fbe8c1005ce3466233f
goorm <goorm@goormui-MacBookPro-5.local>
2024-11-04 01:32:37 +0900

    중복 코드 제거

--- public/js/userManager.js
+++ public/js/userManager.js
@@ -16,51 +16,6 @@ function showToast() {
         }, 1000); // 표시 시간
     }
 }
-
-// 수정하기 버튼 클릭 이벤트
-document.querySelector(".submitButton").addEventListener("click", function (event) {
-    event.preventDefault(); // 폼 제출 방지
-    const nicknameInput = document.getElementById("nickname").value;
-    const errorElement = document.getElementById("nicknameError");
-    errorElement.style.visibility = "visible"; // 헬퍼 텍스트 보이기
-
-    // 닉네임이 비어있는지 체크
-    if (!nicknameInput) {
-        errorElement.textContent = "*닉네임을 입력해주세요.";
-        return;
-    }
-
-    // 닉네임 길이 체크
-    if (nicknameInput.length > 10) {
-        errorElement.textContent = "*닉네임은 최대 10자까지 작성 가능합니다.";
-        return;
-    }
-
-    // NOTE : 임시
-    showToast()
-    
-    // // 서버에 닉네임 검사 요청
-    // fetch('/users/nicknameCheck', { // TODO : 여기에 실제 백엔드 URL로 변경
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({ nickname: nicknameInput }),
-    // })
-    // .then(response => response.json())
-    // .then(data => {
-    //     if (data.error) { // 서버에서 에러가 있는 경우
-    //         errorElement.textContent = data.error; // 에러 메시지 표시
-    //     } else {
-    //         // 닉네임이 유효하면 토스트 메시지 표시
-    //         showToast();
-    //     }
-    // })
-    // .catch((error) => {
-    //     console.error('Error:', error);
-    //     errorElement.textContent = "*서버 오류가 발생했습니다."; // 서버 오류 처리
-    // });
-});
 // 모달 열기 함수
 function openModal() {
     document.getElementById("modalOverlay").style.display = "flex";
@@ -116,13 +71,13 @@ document.addEventListener("DOMContentLoaded", function () {
                         // 이미지 미리보기 업데이트
                         profileImagePreview.src = `http://localhost:2000/images/${file.name}`;
                         profileImagePreview.style.display = "block";
-                        alert('프로필 이미지가 변경되었습니다.');
+                        console.log('프로필 이미지가 변경되었습니다.');
                     } else {
-                        alert('프로필 이미지 변경 실패: ' + result.message);
+                        console.log('프로필 이미지 변경 실패: ' + result.message);
                     }
                 } else {
                     const error = await uploadResponse.json();
-                    alert('프로필 이미지 변경 실패: ' + error.message);
+                    console.log('프로필 이미지 변경 실패: ' + error.message);
                 }
             } catch (error) {
                 console.error('네트워크 오류:', error);
@@ -137,9 +92,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const nickname = document.getElementById("nickname").value;
 
-        // 닉네임 유효성 검사
-        if (!nickname) {
-            alert('닉네임을 입력해주세요.');
+        const nicknameInput = document.getElementById("nickname").value;
+        const errorElement = document.getElementById("nicknameError");
+        errorElement.style.visibility = "visible"; // 헬퍼 텍스트 보이기
+
+        // 닉네임이 비어있는지 체크
+        if (!nicknameInput) {
+            errorElement.textContent = "*닉네임을 입력해주세요.";
+            return;
+        }
+
+        // 닉네임 길이 체크
+        if (nicknameInput.length > 10) {
+            errorElement.textContent = "*닉네임은 최대 10자까지 작성 가능합니다.";
             return;
         }
 
@@ -156,13 +121,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
             if (nicknameResponse.ok) {
                 const result = await nicknameResponse.json();
-                alert('닉네임이 변경되었습니다.');
+                console.log('회원정보가 변경되었습니다.');
+                showToast()
             } else {
                 const error = await nicknameResponse.json();
                 if (nicknameResponse.status === 401) {
-                    alert('*중복된 닉네임입니다.');
+                    console.log('*중복된 닉네임입니다.');
                 } else {
-                    alert('닉네임 변경 실패: ' + error.message);
+                    console.log('회원정보 변경 실패: ' + error.message);
                 }
             }
         } catch (error) {
